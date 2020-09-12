9a7aa169d00d31f325062cfb68dadcb5b89df878
Emirayka <emirayka@protonmail.com>
2020-09-12 17:32:22 +0300

    Updated dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'app'),
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: isProduction ? 'source-map' : 'eval-source-map',
   resolve: {
@@ -37,28 +37,60 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [
-          'style-loader',
-          'css-loader',
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: !isProduction,
+              importLoaders: 1,
+              modules: {
+                localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
+              }
+            }
+          },
           'postcss-loader',
         ],
       },
       {
         test: /\.scss$/i,
         use: [
-          'style-loader',
-          'css-loader',
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: !isProduction,
+              importLoaders: 1,
+              modules: {
+                localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
+              }
+            }
+          },
           'postcss-loader',
           'sass-loader',
         ],
       },
+      { test: /\.(a?png|svg)$/, use: 'url-loader?limit=10000' },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
+        test: /\.(jpe?g|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
+        use: 'file-loader'
+      }
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'app/assets/index.html' }),
+    new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      // For the dev server overlay to work
+      async: false,
+    }),
+    isProduction ? false : new webpack.HotModuleReplacementPlugin(),
+    analyzeBundle ? new BundleAnalyzerPlugin() : false,
+    new MiniCssExtractPlugin({
+      filename: '[hash].css',
+      disable: !isProduction
+    }),
+  ].filter(Boolean),
+  devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'app'),
     port: 9111,
@@ -69,16 +101,4 @@ module.exports = {
     overlay: true,
     stats: 'minimal',
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'app/index.html' }),
-    new ForkTsCheckerWebpackPlugin({
-      // For the dev server overlay to work
-      async: false,
-    }),
-    isProduction ? false : new webpack.HotModuleReplacementPlugin(),
-    analyzeBundle ? new BundleAnalyzerPlugin() : false,
-    new MiniCssExtractPlugin({
-      filename: isProduction ? '[name]-[contenthash].css' : '[name].css',
-    }),
-  ].filter(Boolean),
 }
